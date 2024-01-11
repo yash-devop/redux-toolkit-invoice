ed43697ce16491a7819dd90a6bdcf6ec5f4321f6
yash-devop <kamble1234meena@gmail.com>
2024-01-12 00:37:59 +0530

    added:ui-part2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Image } from "lucide-react";
+import { Image, Send } from "lucide-react";
 import { useState } from "react";
 import AddItem from "./components/AddItem";
 
@@ -13,32 +13,32 @@ function App() {
   }
   return (
     <>
-       <div className="bg-[#f2f3f5e8] overflow-hidden">
-          <div className="flex flex-col lg:flex-row">
+       <div className="bg-[#f2f3f5e8] w-full">
+          <div className="flex flex-col lg:flex-row w-full">
 
               {/* LeftSide box*/}
-              <div className="max-w-[1100px] w-full h-screen p-4 bg-white">
+              <div className="max-w-[1100px] mx-auto w-full h-screen p-4 bg-white">
 
                 {/* Image Upload */}
-                <div className=" flex justify-between items-center gap-3 w-full max-h-[180px] h-full pb-4">
+                <div className=" flex flex-col sm:flex-row sm:justify-between sm:items-center gap-3 w-full sm:max-h-[180px] sm:h-full pb-4">
                     {
                        imageURL ? <img src={imageURL} className="w-[200px] h-[200px] object-contain"/> : (
                           <>
                             <div onClick={()=>document.getElementById("upload_img").click()} className="max-w-[200px] cursor-pointer flex flex-col text-sm gap-3 items-center justify-center w-full h-full border-2 border-dashed border-blue-400 rounded-lg">
                               <Image size={40}/>
                               <p>Upload Logo </p>
-                              <input type="file"  onChange={handleImage} hidden id="upload_img" accept="image/*"/>
+                              <input type="file" onChange={handleImage} hidden id="upload_img" accept="image/*"/>
                             </div>
                           </>
                        )
                     }
 
                     <div className="max-w-[300px] w-full">
-                        <h1 className="font-semibold ">INVOICE</h1>
+                        <h1 className="font-bold text-xl pb-2">INVOICE</h1>
                         <div className="flex flex-col gap-2">
-                          <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg" />
-                          <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg" />
-                          <h1>Invoice Date: 06 May 2024</h1>
+                          <input type="text" placeholder="Company Name"  className="bg-[#eeeeee] focus:bg-transparent border outline-none p-2 rounded-lg" />
+                          <input type="text" placeholder="Customer's Name" className="bg-[#eeeeee] focus:bg-transparent border outline-none p-2 rounded-lg" />
+                          <h1 className="font-semibold">Invoice Date: <span className="text-gray-400">06 May 2024</span></h1>
 
                         </div>
                     </div>
@@ -49,18 +49,14 @@ function App() {
                 {/* Content */}
                 <div className="pt-4 flex items-center justify-between gap-2 ">
                    <div className="flex flex-col gap-2 w-full">
-                      <label>Billing Address</label>
-                      <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg w-full" />
-                      <label>Billing Address</label>
-                      <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg w-full" />
+                      <label className="font-medium">Billing To Address</label>
+                      <input type="text" placeholder="Who is this invoice to ?" className="bg-[#eeeeee] focus:bg-transparent border outline-none p-3 rounded-lg w-full" />
                    </div>
 
 
                    <div className="flex flex-col gap-2 w-full">
-                      <label>Billing Address</label>
-                      <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg w-full" />
-                      <label>Billing Address</label>
-                      <input type="text" placeholder="Who is this invoice to ?" className="border outline-none p-3 rounded-lg w-full" />
+                      <label className="font-medium">Billing From Address</label>
+                      <input type="text" placeholder="Who is this invoice to ?" className="bg-[#eeeeee] focus:bg-transparent border outline-none p-3 rounded-lg w-full" />
                    </div>
 
                 </div>
@@ -74,8 +70,14 @@ function App() {
 
 
               {/* RightSide box*/}
-              <div className="h-screen w-fit p-4">
-                <h1>right</h1>
+              <div className="flex flex-col items-center gap-2 h-screen max-w-[400px] w-full mx-auto p-4">
+                 <button className="p-3  font-bold w-full border border-black text-black">
+                    Preview
+                 </button>
+                 <button className="flex items-center justify-center gap-3 p-3 text-white font-bold w-full bg-blue-600">
+                    <Send size={20}/>
+                    Send Invoice
+                 </button>
               </div>
 
           </div>
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -3,23 +3,54 @@ import React, { useState } from 'react'
 
 const AddItem = () => {
   const [items , setItems] = useState([]);
+
+
+
+  const [product , setProducts] = useState({
+    productName : "",
+    quantity: 1,
+    price: 0
+  })
+
+
+
   const handleAddItem=()=>{
-      console.log("s");
+     setItems([...items , product])
   }
-  const handleRemoveItem=()=>{
-      console.log("s");
+  const handleRemoveItem=(index)=>{
+    const newItems = [...items];
+    newItems.splice(index, 1); // Remove the item at the specified index
+    setItems(newItems);
   }
   return (
     <>
-        <div className='flex items-center justify-between gap-4'>
-            <input type="text" placeholder='Enter your item' className='border outline-none w-full p-3 rounded-lg'/>
+        {
+            items.map((item , index)=>(
+                <>
+                    <div className='flex gap-4 items-center'>
+                        <p>{index + 1}</p>
+                        <input type="text" value={item?.productName} disabled className='w-full p-3'/>
+                        <div onClick={()=>handleRemoveItem(index)} className='bg-red-400 hover:opacity-90 cursor-pointer flex items-center justify-center rounded-lg text-white  min-w-[40px] h-[40px]'>
+                        <Trash2 size={20}/>
+                    </div>
+                </div>
+                </>
+            ))
+        }
+        <div className='flex items-center justify-between gap-6'>
+            <input onChange={(e)=>setProducts({...product , productName: e.target.value})} type="text" value={product.productName} placeholder='Add Item' className='border outline-none w-full p-3 rounded-lg'/>
             <div className='flex items-center gap-4'>
-                <div onClick={handleAddItem} className='bg-emerald-400 hover:opacity-90 cursor-pointer  flex items-center justify-center rounded-lg text-white min-w-[50px] h-[50px]'>
+                <label>
+                    <h1>Quantity</h1>
+                </label>
+                <input type="number" defaultValue={1} className='border min-w-[20px] w-full outline-none p-2' />
+                <label>
+                    <h1>Price</h1>
+                </label>
+                <input type="number" defaultValue={1} className='border min-w-[80px] w-full outline-none p-2' />
+                <button onClick={handleAddItem} disabled={product.productName.trim() === "" ? true : false}  className='disabled:bg-gray-400 bg-emerald-400 hover:opacity-90 cursor-pointer  flex items-center justify-center rounded-lg text-white min-w-[40px] h-[40px]'>
                     <Plus size={23}/>    
-                </div>
-                <div onClick={handleRemoveItem} className='bg-red-400 hover:opacity-90 cursor-pointer flex items-center justify-center rounded-lg text-white  min-w-[50px] h-[50px]'>
-                    <Trash2 size={23}/>
-                </div>
+                </button>
             </div>
         </div>
     </>
